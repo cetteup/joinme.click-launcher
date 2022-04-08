50146bbd4b1927732dd0341a390cf85f0ac0ad98
cetteup <me@cetteup.com>
2022-04-09 00:43:36 +0200

    feat: Add support for Call of Duty 4: Modern Warfare

---
+++ game/titles/cod4.go
@@ -0,0 +1,18 @@
+package titles
+
+import (
+	"fmt"
+	"github.com/cetteup/joinme.click-launcher/game"
+)
+
+var Cod4Config = game.LauncherConfig{
+	ProtocolScheme:    "cod4",
+	GameLabel:         "Call of Duty 4: Modern Warfare",
+	ExecutablePath:    "iw3mp.exe",
+	RegistryPath:      "SOFTWARE\\WOW6432Node\\Activision\\Call of Duty 4",
+	RegistryValueName: "InstallPath",
+}
+
+var Cod4CmdBuilder game.CommandBuilder = func(config game.LauncherConfig, ip string, port string) ([]string, error) {
+	return []string{"+connect", fmt.Sprintf("%s:%s", ip, port)}, nil
+}
