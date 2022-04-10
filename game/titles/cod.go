e7f9ca763a3c4813e975a1ce60d10ffc123473f5
cetteup <me@cetteup.com>
2022-04-10 23:42:19 +0200

    refactor: Move Call of Duty: United Offensive to same file as base game

--- game/titles/cod.go
+++ game/titles/cod.go
@@ -9,3 +9,11 @@ var CodConfig = game.LauncherConfig{
 	RegistryPath:      "SOFTWARE\\WOW6432Node\\Activision\\Call of Duty",
 	RegistryValueName: "InstallPath",
 }
+
+var CodUOConfig = game.LauncherConfig{
+	ProtocolScheme:    "coduo",
+	GameLabel:         "Call of Duty: United Offensive",
+	ExecutablePath:    "CoDUOMP.exe",
+	RegistryPath:      "SOFTWARE\\WOW6432Node\\Activision\\Call of Duty United Offensive",
+	RegistryValueName: "InstallPath",
+}
