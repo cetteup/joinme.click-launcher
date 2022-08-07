8479b793deafe3dfe25cd58ab36a010d65810081
cetteup <me@cetteup.com>
2022-08-07 16:39:51 +0200

    test: Add remaining tests for GameTitle and GameMod

--- internal/domain/game_title_test.go
+++ internal/domain/game_title_test.go
@@ -139,3 +139,246 @@ func TestGameTitle_AddCustomConfig(t *testing.T) {
 		})
 	}
 }
+
+func TestGameTitle_RequiresPlatformClient(t *testing.T) {
+	type test struct {
+		name                       string
+		givenTitle                 GameTitle
+		wantRequiresPlatformClient bool
+	}
+
+	tests := []test{
+		{
+			name: "true for game title with platform client",
+			givenTitle: GameTitle{
+				PlatformClient: &PlatformClient{
+					Platform: "some-platform-client",
+				},
+			},
+			wantRequiresPlatformClient: true,
+		},
+		{
+			name:                       "false for game title without platform client",
+			givenTitle:                 GameTitle{},
+			wantRequiresPlatformClient: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// WHEN
+			requiresPlatformClient := tt.givenTitle.RequiresPlatformClient()
+
+			// THEN
+			assert.Equal(t, tt.wantRequiresPlatformClient, requiresPlatformClient)
+		})
+	}
+}
+
+func TestGameTitle_GetMod(t *testing.T) {
+	type test struct {
+		name        string
+		givenTitle  GameTitle
+		givenSlug   string
+		expectedMod *GameMod
+	}
+
+	tests := []test{
+		{
+			name: "successfully returns mod",
+			givenTitle: GameTitle{
+				Mods: []GameMod{
+					{
+						Name:          "some-mod",
+						Slug:          "some-mod-slug",
+						finderConfigs: nil,
+					},
+				},
+			},
+			givenSlug: "some-mod-slug",
+			expectedMod: &GameMod{
+				Name:          "some-mod",
+				Slug:          "some-mod-slug",
+				finderConfigs: nil,
+			},
+		},
+		{
+			name: "ignores case when comparing slugs",
+			givenTitle: GameTitle{
+				Mods: []GameMod{
+					{
+						Name:          "some-mod",
+						Slug:          "soMe-moD-slUG",
+						finderConfigs: nil,
+					},
+				},
+			},
+			givenSlug: "SOmE-mOd-SLug",
+			expectedMod: &GameMod{
+				Name:          "some-mod",
+				Slug:          "soMe-moD-slUG",
+				finderConfigs: nil,
+			},
+		},
+		{
+			name: "nil for unsupported mod",
+			givenTitle: GameTitle{
+				Mods: []GameMod{
+					MakeMod("some-mod", "some-mod-slug", nil),
+				},
+			},
+			givenSlug:   "some-other-mod-slug",
+			expectedMod: nil,
+		},
+		{
+			name:        "nil for title without mods",
+			givenTitle:  GameTitle{},
+			givenSlug:   "some-mod-slug",
+			expectedMod: nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// WHEN
+			mod := tt.givenTitle.GetMod(tt.givenSlug)
+
+			// THEN
+			assert.Equal(t, tt.expectedMod, mod)
+		})
+	}
+}
+
+func TestGameTitle_String(t *testing.T) {
+	type test struct {
+		name           string
+		givenTitle     *GameTitle
+		expectedString string
+	}
+
+	tests := []test{
+		{
+			name: "returns game name and protocol scheme for given title",
+			givenTitle: &GameTitle{
+				Name:           "some-game",
+				ProtocolScheme: "some-game-protocol",
+			},
+			expectedString: "some-game (some-game-protocol)",
+		},
+		{
+			name:           "returns nil as string for nil title",
+			givenTitle:     nil,
+			expectedString: "nil",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// WHEN
+			asString := tt.givenTitle.String()
+
+			// THEN
+			assert.Equal(t, tt.expectedString, asString)
+		})
+	}
+}
+
+func TestGameMod_ComputeFinderConfigs(t *testing.T) {
+	type test struct {
+		name                  string
+		givenMod              GameMod
+		givenGameInstallPath  string
+		expectedFinderConfigs []software_finder.Config
+	}
+
+	tests := []test{
+		{
+			name: "successfully computes finder configs",
+			givenMod: GameMod{
+				Name: "some-mod",
+				Slug: "some-mod-slug",
+				finderConfigs: []software_finder.Config{
+					{
+						ForType:     software_finder.PathFinder,
+						InstallPath: "mods\\some-mod\\objects.zip",
+						PathType:    software_finder.PathTypeFile,
+					},
+				},
+			},
+			givenGameInstallPath: "C:\\Games\\Battlefield",
+			expectedFinderConfigs: []software_finder.Config{
+				{
+					ForType:     software_finder.PathFinder,
+					InstallPath: "C:\\Games\\Battlefield\\mods\\some-mod\\objects.zip",
+					PathType:    software_finder.PathTypeFile,
+				},
+			},
+		},
+		{
+			name: "ignores finder configs not using path finder",
+			givenMod: GameMod{
+				Name: "some-mod",
+				Slug: "some-mod-slug",
+				finderConfigs: []software_finder.Config{
+					{
+						ForType:           software_finder.RegistryFinder,
+						RegistryPath:      "some-game\\some-mod",
+						RegistryValueName: "InstallPath",
+					},
+				},
+			},
+			givenGameInstallPath: "C:\\Games\\Battlefield",
+			expectedFinderConfigs: []software_finder.Config{
+				{
+					ForType:           software_finder.RegistryFinder,
+					RegistryPath:      "some-game\\some-mod",
+					RegistryValueName: "InstallPath",
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// WHEN
+			finderConfigs := tt.givenMod.ComputeFinderConfigs(tt.givenGameInstallPath)
+
+			// THEN
+			assert.Equal(t, tt.expectedFinderConfigs, finderConfigs)
+		})
+	}
+}
+
+func TestGameMod_String(t *testing.T) {
+	type test struct {
+		name           string
+		givenMod       *GameMod
+		expectedString string
+	}
+
+	tests := []test{
+		{
+			name: "returns mod name and slug for given mod",
+			givenMod: &GameMod{
+				Name: "some-mod",
+				Slug: "some-mod-slug",
+			},
+			expectedString: "some-mod (some-mod-slug)",
+		},
+		{
+			name:           "returns nil as string for nil title",
+			givenMod:       nil,
+			expectedString: "nil",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// WHEN
+			asString := tt.givenMod.String()
+
+			// THEN
+			assert.Equal(t, tt.expectedString, asString)
+		})
+	}
+}
