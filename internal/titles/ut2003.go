a6bd738e4c866372c3c3d7662387ac4fb90a1d03
cetteup <me@cetteup.com>
2023-01-16 20:40:00 +0100

    feat: Add support for Unreal Tournament 2003

---
+++ internal/titles/ut2003.go
@@ -0,0 +1,28 @@
+package titles
+
+import (
+	"github.com/cetteup/joinme.click-launcher/internal/domain"
+	"github.com/cetteup/joinme.click-launcher/internal/titles/internal"
+	"github.com/cetteup/joinme.click-launcher/pkg/game_launcher"
+	"github.com/cetteup/joinme.click-launcher/pkg/software_finder"
+)
+
+var UT2003 = domain.GameTitle{
+	Name:           "Unreal Tournament 2003",
+	ProtocolScheme: "ut2003",
+	FinderConfigs: []software_finder.Config{
+		{
+			ForType:           software_finder.RegistryFinder,
+			RegistryKey:       software_finder.RegistryKeyLocalMachine,
+			RegistryPath:      "SOFTWARE\\WOW6432Node\\Unreal Technology\\Installed Apps\\UT2003",
+			RegistryValueName: "Folder",
+		},
+	},
+	LauncherConfig: game_launcher.Config{
+		ExecutableName:    "UT2003.exe",
+		ExecutablePath:    "System",
+		CloseBeforeLaunch: true,
+	},
+	URLValidator: internal.IPPortURLValidator,
+	CmdBuilder:   internal.PlainCmdBuilder,
+}
