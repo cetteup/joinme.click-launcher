cdd32d0e0b3e6f2f57ef1aae4812e5d60d53ecc8
cetteup <me@cetteup.com>
2023-02-09 21:36:49 +0100

    feat: Add disk version path for Unreal Tournament

--- internal/titles/ut.go
+++ internal/titles/ut.go
@@ -15,13 +15,19 @@ var UT = domain.GameTitle{
 			ForType:           software_finder.RegistryFinder,
 			RegistryKey:       software_finder.RegistryKeyLocalMachine,
 			RegistryPath:      "SOFTWARE\\Unreal Technology\\Installed Apps\\UnrealTournament",
-			RegistryValueName: "folder",
+			RegistryValueName: "Folder",
 		},
 		{
 			ForType:           software_finder.RegistryFinder,
 			RegistryKey:       software_finder.RegistryKeyCurrentUser, // When installed via Steam, key is CurrentUser instead of LocalMachine
 			RegistryPath:      "SOFTWARE\\Unreal Technology\\Installed Apps\\UnrealTournament",
-			RegistryValueName: "folder",
+			RegistryValueName: "Folder",
+		},
+		{
+			ForType:           software_finder.RegistryFinder,
+			RegistryKey:       software_finder.RegistryKeyLocalMachine,
+			RegistryPath:      "SOFTWARE\\WOW6432Node\\Unreal Technology\\Installed Apps\\UnrealTournament", // Disk version uses WOW6432Node
+			RegistryValueName: "Folder",
 		},
 	},
 	LauncherConfig: game_launcher.Config{
